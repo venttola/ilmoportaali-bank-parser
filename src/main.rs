8c2e1e09f27d25988cbc97dc3e96d2350384dcc7
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2019-10-09 21:29:31 +0300

    Create CLI and output in .csv spec

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
+mod cli;
 mod dom;
 mod nda;
 
-use serde_json::Error;
+use cli::{cli, ARGNAME_BANKDATA, ARGNAME_ILMODATA};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
-fn main() -> Result<(), Error> {
-    let payments_by_ref = {
-        let nda_file = include_str!("../example_data/bankdata.NDA");
-        nda::scrape_payments(nda_file)
-    };
+fn main() {
+    // read command line arguments
+    let matches = cli();
 
-    let enrolment_info = include_str!("../example_data/ilmodata.json");
-    let parsed: Vec<dom::Group> = serde_json::from_str(enrolment_info)?;
+    // read json file into enrolment info by group
+    let enrolment_file = matches.value_of(ARGNAME_ILMODATA).unwrap();
+    let groups = enrolment_info_from_json_file(enrolment_file);
 
-    for group in &parsed {
+    // read nda file into payments by reference number (viitenumero)
+    let payments_file = matches.value_of(ARGNAME_BANKDATA).unwrap();
+    let payments_by_ref = bankdata_from_nda_file(payments_file);
+
+    println!("group_name, missing_payments_eur");
+    for group in &groups {
         let name = &group.meta.name;
         let required = group.total_sum;
         let ref_number = &group.ref_number;
@@ -23,14 +32,33 @@ fn main() -> Result<(), Error> {
             }
         };
 
-        println!(
-            "Ryhmän '{}' pitäisi maksaa {} €. Ryhmä on maksanut {} €. Erotus: {} €.",
-            name,
-            required,
-            payments,
-            payments - required
-        );
+        let to_be_paid = required - payments;
+        if to_be_paid != 0f64 {
+            println!("{}, {}", name, to_be_paid);
+        }
     }
+}
 
-    Ok(())
+/// Scrapes payments by reference number (viitenumero) from given .NDA file.
+fn bankdata_from_nda_file(filename: &str) -> HashMap<String, f64> {
+    let path = Path::new(filename);
+    let mut file = File::open(path).expect(&format!("cannot open file {:?}", path));
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect(&format!("cannot read file {:?}", path));
+
+    // scrape
+    nda::scrape_payments(contents)
+}
+
+/// Deserializes enrolment information from a JSON source.
+fn enrolment_info_from_json_file(filename: &str) -> Vec<dom::Group> {
+    let path = Path::new(filename);
+    let mut file = File::open(path).expect(&format!("cannot open file {:?}", path));
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect(&format!("cannot read file {:?}", path));
+
+    // deserialize
+    serde_json::from_str(&contents).expect("contents of {:?} do not parse into valid JSON")
 }
